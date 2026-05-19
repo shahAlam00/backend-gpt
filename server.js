573c42f4f9f0bb7b47e3d775ae3d1f9cb8918fe9
Shah Alam <shahsaifi8279@gmail.com>
2026-05-19 04:44:52 -0700

    fix: update response message in root endpoint

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.listen(PORT, () => {
 });
 
 app.get("/", (req, res) => {
-    res.send("Update :test was successful");
+    res.send("Update Shah rukh:test was successful");
 });
 const connectDB = async () => {
     try {
