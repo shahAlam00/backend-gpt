bd049b4fb247baf6ea2a5b22b0b96f1cbe96ad25
Shah Alam <shahsaifi8279@gmail.com>
2026-06-10 04:22:59 -0700

    fix: correct spacing in homepage response message

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.listen(PORT, () => {
 });
 
 app.get("/", (req, res) => {
-    res.send("Hllo Bro ,I am Shah Alam Software Engineer and I am learning MERN Stack Development");
+    res.send("Hllo  ,I am Shah Alam Software Engineer and I am learning MERN Stack Development");
 });
 const connectDB = async () => {
     try {
